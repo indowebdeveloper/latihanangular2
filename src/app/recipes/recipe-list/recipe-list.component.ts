373124e9672e18744a9fbe18fa4cea9ea753e254
Indowebdeveloper <git@indowebdeveloper.com>
2020-12-21 21:25:15 +0700

    asdasd

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Recipe } from './recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe()];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
